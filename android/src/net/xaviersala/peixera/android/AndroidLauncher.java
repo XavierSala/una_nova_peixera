ea2db1ad9a06f49f9208b9d917823558b079efb6
Xavier Sala <utrescu@gmail.com>
2015-11-03 17:28:00 +0100

    Primer commit

---
+++ android/src/net/xaviersala/peixera/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.xaviersala.peixera.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.xaviersala.peixera.PeixeraGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PeixeraGame(), config);
+	}
+}
---
+++ desktop/src/net/xaviersala/peixera/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.xaviersala.peixera.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.xaviersala.peixera.PeixeraGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PeixeraGame(), config);
+	}
+}
